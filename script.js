ea0298010d59dcc4c35dc9c9b0c8e03bb2e79acb
TerryFX <korean1790@gmail.com>
2023-02-28 00:51:53 +0900

    Increase Resolution & Movable Node

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+const ResolutionValue = 1600 / 800
+const Width = canvas.width * ResolutionValue
+const Height = canvas.height * ResolutionValue
+
 const PI2 = Math.PI * 2
 const MaxNodeCount = 5
 const NodeSize = 5
+const SelectSize = 20
 const ConnectRange = 200
+const CountOfNode = 50
+const HoverBigger = 10
+
+let SelectedNode = undefined
+let HoverNode = undefined
 
 function distance(A, B) {
     return Math.sqrt((A.x - B.x) ** 2 + (A.y - B.y) ** 2)
@@ -15,6 +25,13 @@ function random(m) {
 }
 
 const print = (text) => { console.log(text) }
+
+class Vector {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+}
 
 class Node {
     constructor(x, y, r) {
@@ -54,8 +71,8 @@ class Node {
 }
 
 const renderList = []
-for(var i =0; i<20; i++){
-    renderList.push(new Node(random(500) + 100, random(500) + 100, NodeSize))
+for (var i = 0; i < CountOfNode; i++) {
+    renderList.push(new Node(random(1000) + 100, random(1000) + 100, NodeSize))
 }
 
 for (var i = 0; i < renderList.length; i++) {
@@ -72,9 +89,53 @@ for (var i = 0; i < renderList.length; i++) {
 }
 
 function render() {
+    ctx.clearRect(0, 0, Width, Height)
     for (var i = 0; i < renderList.length; i++) {
         renderList[i].draw()
     }
     requestAnimationFrame(render)
 }
+
 render()
+
+canvas.addEventListener("mousemove", (e) => {
+    if (SelectedNode !== undefined) {
+        SelectedNode.x = e.offsetX * ResolutionValue
+        SelectedNode.y = e.offsetY * ResolutionValue
+        return
+    }
+    const pos = new Vector(e.offsetX * ResolutionValue, e.offsetY * ResolutionValue)
+    if (HoverNode !== undefined && distance(HoverNode, pos) > HoverNode.r) {
+        HoverNode.r = NodeSize
+        HoverNode = undefined
+    }
+    for (var i = 0; i < renderList.length; i++) {
+        if (HoverNode == renderList[i]) {
+            continue
+        }
+        if (distance(renderList[i], pos) <= SelectSize) {
+            if (HoverNode !== undefined) {
+                HoverNode.r = NodeSize
+            }
+            HoverNode = renderList[i]
+            HoverNode.r = NodeSize + HoverBigger
+            break
+        }
+    }
+})
+
+canvas.addEventListener("mousedown", (e) => {
+    const pos = new Vector(e.offsetX * ResolutionValue, e.offsetY * ResolutionValue)
+    for (var i = 0; i < renderList.length; i++) {
+        if (distance(renderList[i], pos) <= SelectSize) {
+            SelectedNode = renderList[i]
+            break
+        }
+    }
+})
+
+canvas.addEventListener("mouseup", (e) => {
+    if (SelectedNode !== undefined) {
+        SelectedNode = undefined
+    }
+})
